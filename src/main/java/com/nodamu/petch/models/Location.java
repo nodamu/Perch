ecabca09f8901bfbeb544d5d9af03209d16c29ac
oboi_niqo <nickadamu@gmail.com>
2021-03-18 09:42:43 +0000

    Fixed typo

--- src/main/java/com/nodamu/petch/models/Location.java
+++ src/main/java/com/nodamu/petch/models/Location.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @NoArgsConstructor
 @Accessors(chain = true)
-@Document(collation = "LocationInfo")
+@Document(collection = "LocationInfo")
 public class Location {
     @Id
     private String Id;
