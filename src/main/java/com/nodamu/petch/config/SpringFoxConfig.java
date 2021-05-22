9f55150dfb018356e2a6a16d01409c591b560176
oboi_niqo <nickadamu@gmail.com>
2021-05-22 23:11:18 +0000

    configs

--- src/main/java/com/nodamu/petch/config/SpringFoxConfig.java
+++ src/main/java/com/nodamu/petch/config/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package com.nodamu.petch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
--- src/main/java/com/nodamu/petch/exceptions/PropertyDoesNotExistException.java
+++ src/main/java/com/nodamu/petch/exceptions/PropertyDoesNotExistException.java
@@ -11,4 +11,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PropertyDoesNotExistException extends RuntimeException{
 
+
 }
